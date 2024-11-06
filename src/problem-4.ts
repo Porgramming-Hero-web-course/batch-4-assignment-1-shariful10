079b1494a17f55cea93481b6445dcaa6da3f4611
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-11-06 23:06:43 +0600

    Problem 4 & 5 solved

---
+++ src/problem-4.ts
@@ -0,0 +1,34 @@
+type Circle = {
+	shape: "circle";
+	radius: number;
+};
+
+type Rectangle = {
+	shape: "rectangle";
+	width: number;
+	height: number;
+};
+
+type Shape = Circle | Rectangle;
+
+function calculateShapeArea(shape: Shape): number {
+	let area: number;
+	if (shape.shape === "circle") {
+		area = Math.PI * shape.radius * shape.radius;
+	} else {
+		area = shape.width * shape.height;
+	}
+
+	return Number(area);
+}
+
+const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+console.log(circleArea.toFixed(2));
+
+const rectangleArea = calculateShapeArea({
+	shape: "rectangle",
+	width: 4,
+	height: 6,
+});
+
+console.log(rectangleArea);
